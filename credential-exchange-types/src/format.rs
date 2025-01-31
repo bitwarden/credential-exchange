85f3eee8de6aa4fe526b26170561e9b5aae77d02
Hinton <hinton@users.noreply.github.com>
2025-01-31 13:25:55 +0100

    Add CredentialScope

--- credential-exchange-types/src/format.rs
+++ credential-exchange-types/src/format.rs
@@ -13,7 +13,7 @@ pub use self::{
     },
     passkey::{Fido2Extensions, Fido2HmacSecret, Fido2LargeBlob, Fido2SupplementalKeys},
 };
-use crate::b64url::B64Url;
+use crate::{b64url::B64Url, Uri};
 
 mod document;
 mod identity;
@@ -130,6 +130,10 @@ pub struct Item<E = ()> {
     /// present in the UI.
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub favorite: Option<bool>,
+    /// This OPTIONAL member defines the scope where the [Item::credentials] SHOULD be presented.
+    /// The credentials SHOULD only be presented within this scope unless otherwise specified by a
+    /// specific [Credential] type.
+    pub scope: CredentialScope,
     /// This member contains a set of [Credentials][Item::credentials] that SHOULD be associated to
     /// the type.
     pub credentials: Vec<Credential>,
@@ -273,4 +277,57 @@ pub enum FieldType {
     Date,
     #[serde(untagged)]
     Unknown(String),
+}
+
+/// This is an object that describes an appropriate context in which the [Item]'s
+/// [Item::credentials] can to be used.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CredentialScope {
+    /// This member holds strings which SHOULD follow the Uniform Resource Identifier (URI) syntax
+    /// as defined in [RFC3986](https://www.rfc-editor.org/rfc/rfc3986).
+    pub urls: Vec<Uri>,
+    /// This member defines the android apps that have been validated to be appropriate for the
+    /// credentials to be used.
+    pub android_apps: Vec<AndroidAppIdCredential>,
+}
+
+/// An [AndroidAppIdCredential] contains the information required to verify and identify an
+/// [Android](https://www.android.com/) application for automatically filling other credentials
+/// associated to the same [Item] as this one.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AndroidAppIdCredential {
+    /// The application identifier. A non-normative example of an application identifier is
+    /// `"com.example.myapp"`.
+    pub bundle_id: String,
+    /// The fingerprint of the public certificate used to sign the android application. This member
+    /// is OPTIONAL but is highly recommended to be stored for validation during an autofill
+    /// operation.
+    pub certificate: Option<AndroidAppCertificateFingerprint>,
+    /// The [human-palatable](https://www.w3.org/TR/webauthn-3/#human-palatability) name for the
+    /// application, this can be fetched from the android system when associating the app to an
+    /// item. It is highly recommended for providers to store this name.
+    pub name: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AndroidAppCertificateFingerprint {
+    /// This is the hash of the application's public certificate using the hashing algorithm
+    /// defined in [AndroidAppCertificateFingerprint::hash_algorithm]. The bytes of the hash are
+    /// then encoded into base64url directly.
+    pub fingerprint: B64Url,
+    /// The algorithm used to hash the [AndroidAppCertificateFingerprint::fingerprint]. This SHOULD
+    /// be of value [AndroidAppHashAlgorithm].
+    pub hash_algorithm: AndroidAppHashAlgorithm,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum AndroidAppHashAlgorithm {
+    Sha256,
+    Sha1,
+    #[serde(untagged)]
+    Other(String),
 }
